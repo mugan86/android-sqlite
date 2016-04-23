6ef97b4c9cbc4f543037953fafafaf92728a8555
anartzmugika <mugan86@gmail.com>
2016-04-23 20:38:14 +0200

    Remove unused constants

--- app/src/main/java/com/amugika/sqlite/Constants.java
+++ app/src/main/java/com/amugika/sqlite/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
 
     //Bundle key strings
 
-    public static final String MOUNTAIN_ITEM_URL = "mountain_item_url";
+    public static final String RACE_ITEM_URL = "race_item_url";
 
     //Directory constants
 
@@ -27,56 +27,6 @@ public class Constants {
 
 
     //SQLite Database local constants
-
-    /**
-     * Table name constant
-     */
-    public static final String C_TABLE_MOUNTAIN = "mountain" ;
-    public static final String C_TABLE_CLIMBS = "climbs" ;
-    /**
-     * Table columns constants (MOUNTAINS)
-     */
-    public static final String C_COLUMN_ID   = "_id"; //Xehetasunak Mendiarenak
-    public static final String C_COLUMN_ALTITUDE = "altitude";
-    public static final String C_COLUMN_CITY   = "city";
-    public static final String C_COLUMN_CODE = "code"; //Excel ID
-    public static final String C_COLUMN_COMMENT = "comment";
-    public static final String C_COLUMN_EHUN_MENDI = "ehun_mendi";
-    public static final String C_COLUMN_EXTRA_PHOTO = "extraphoto";
-    public static final String C_COLUMN_FOR_CHILDREN = "for_children";
-    public static final String C_COLUMN_IMPORTANCE = "importance";
-    public static final String C_COLUMN_INACTIVE = "inactive";
-    public static final String C_COLUMN_ITEMURL = "itemurl";
-    public static final String C_COLUMN_LAT = "lat";
-    public static final String C_COLUMN_LON = "lon";
-    public static final String C_COLUMN_MAIN_PHOTO = "mainphoto";
-    public static final String C_COLUMN_MAIN_PHOTO_ID = "mainphoto_id";
-    public static final String C_COLUMN_MENDIKAT_PROVINCE = "mendikat_province";
-    public static final String C_COLUMN_MOUNTAIN_RANGE = "mountain_range";
-    public static final String C_COLUMN_NAME = "name";
-    public static final String C_COLUMN_PROVINCE = "province";
-
-    public static final String [] COLUMNS_MOUNTAINS = new String[]{
-            Constants.C_COLUMN_ID,
-            Constants.C_COLUMN_ALTITUDE,
-            Constants.C_COLUMN_CITY,
-            Constants.C_COLUMN_CODE,
-            Constants.C_COLUMN_COMMENT,
-            Constants.C_COLUMN_EHUN_MENDI,
-            Constants.C_COLUMN_EXTRA_PHOTO,
-            Constants.C_COLUMN_FOR_CHILDREN,
-            Constants.C_COLUMN_IMPORTANCE,
-            Constants.C_COLUMN_INACTIVE,
-            Constants.C_COLUMN_ITEMURL,
-            Constants.C_COLUMN_LAT,
-            Constants.C_COLUMN_LON,
-            Constants.C_COLUMN_MAIN_PHOTO,
-            Constants.C_COLUMN_MAIN_PHOTO_ID,
-            Constants.C_COLUMN_MENDIKAT_PROVINCE,
-            Constants.C_COLUMN_MOUNTAIN_RANGE,
-            Constants.C_COLUMN_NAME,
-            Constants.C_COLUMN_PROVINCE
-    } ;
 
     /**
      * Table name constant
