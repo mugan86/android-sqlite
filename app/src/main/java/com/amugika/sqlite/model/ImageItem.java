f8be72d608454538e7e5b4526cb9ee599004db57
Anartz Mugika <mugan86@gmail.com>
2016-05-31 10:12:53 +0200

    Image Item to manage gallery list

--- app/src/main/java/com/amugika/sqlite/model/ImageItem.java
+++ app/src/main/java/com/amugika/sqlite/model/ImageItem.java
@@ -1,7 +1,33 @@
 package com.amugika.sqlite.model;
 
+import android.graphics.Bitmap;
+
 /**
  * Created by anartzmugika on 31/5/16.
  */
 public class ImageItem {
+    private Bitmap image;
+    private String title;
+
+    public ImageItem(Bitmap image, String title) {
+        super();
+        this.image = image;
+        this.title = title;
+    }
+
+    public Bitmap getImage() {
+        return image;
+    }
+
+    public void setImage(Bitmap image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
