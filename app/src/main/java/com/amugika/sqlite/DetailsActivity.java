883667d557affc9dac76144c6635ef733c8bb353
Anartz Mugika <mugan86@gmail.com>
2016-05-31 10:12:14 +0200

    Add Details Activity

--- app/src/main/java/com/amugika/sqlite/DetailsActivity.java
+++ app/src/main/java/com/amugika/sqlite/DetailsActivity.java
@@ -1,7 +1,31 @@
 package com.amugika.sqlite;
 
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.amugika.sqlite.utils.Actions;
+import com.amugika.sqlite.utils.TouchImageView;
+
 /**
  * Created by anartzmugika on 31/5/16.
  */
-public class DetailsActivity {
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.details_activity);
+
+        String title = getIntent().getStringExtra("title");
+        Bitmap bitmap = Actions.getBitmapFromURL(getIntent().getStringExtra("image"));
+
+        TextView titleTextView = (TextView) findViewById(R.id.title);
+        titleTextView.setText(title);
+
+        TouchImageView image = (TouchImageView) findViewById(R.id.image);
+        image.setImageBitmap(bitmap);
+
+    }
 }
